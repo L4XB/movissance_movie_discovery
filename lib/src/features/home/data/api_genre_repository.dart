9b2cd4eb6b25211fae317400f75f9156788c6e45
L4XB <lukas.buck@e-mail.de>
2025-02-15 14:30:32 +0000

    core: added functionallity to load genres

--- lib/src/features/home/data/api_genre_repository.dart
+++ lib/src/features/home/data/api_genre_repository.dart
@@ -1,10 +1,22 @@
+import 'package:dio/dio.dart';
+import 'package:red_line/src/config.dart';
 import 'package:red_line/src/features/home/data/genre_repository.dart';
 import 'package:red_line/src/features/home/domain/genre_model.dart';
 
 class ApiGenreRepository implements GenreRepository {
   @override
-  Future<List<GenreModel>> getAllGenres() {
-    // TODO: implement getAllGenres
-    throw UnimplementedError();
+  Future<List<GenreModel>> getAllGenres() async {
+    final Response response = await Dio().get(
+        "$theMovieDatabaseApiBaseURL/genre/movie/list",
+        queryParameters: {"api_key": theMovieDatabaseApiKey});
+
+    if (response.statusCode == 200 || response.statusCode == 201) {
+      final List<GenreModel> genres = (response.data["genres"] as List)
+          .map((genre) => GenreModel.fromJson(genre))
+          .toList();
+      return genres;
+    } else {
+      throw Exception("Failed to load genres");
+    }
   }
 }
