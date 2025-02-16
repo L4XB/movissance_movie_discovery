cbe70d11190405d51f88aad80dfb3e3bdcf919fc
L4XB <lukas.buck@e-mail.de>
2025-02-16 10:59:47 +0000

    core: implemented search functionallity

--- lib/src/features/home/bloc/home_movie_cubit/home_movie_cubit.dart
+++ lib/src/features/home/bloc/home_movie_cubit/home_movie_cubit.dart
@@ -1,5 +1,6 @@
 import 'package:bloc/bloc.dart';
 import 'package:meta/meta.dart';
+import 'package:red_line/src/common/utils/string_formater.dart';
 import 'package:red_line/src/features/home/data/api_movie_repository.dart';
 import 'package:red_line/src/features/home/data/movie_repository.dart';
 import 'package:red_line/src/features/home/domain/movie_model.dart';
@@ -25,6 +26,19 @@ class HomeMovieCubit extends Cubit<HomeMovieState> {
     emit(HomeMovieLoadingState());
     try {
       final movies = await movieRepository.discoverMovies(page, genreId, null);
+      emit(HomeMovieLoadedState(movies, page));
+    } catch (e) {
+      emit(HomeMovieErrorState(e.toString()));
+    }
+  }
+
+  void loadMoviesBySearch(String text, int page) async {
+    emit(HomeMovieLoadingState());
+    try {
+      final query = StringFormater.textToQuery(text);
+      print(query);
+      final movies = await movieRepository.searchMovieByName(query, page);
+      print(movies);
       emit(HomeMovieLoadedState(movies, page));
     } catch (e) {
       emit(HomeMovieErrorState(e.toString()));
--- lib/src/features/home/presentation/widgets/searchfield_widget.dart
+++ lib/src/features/home/presentation/widgets/searchfield_widget.dart
@@ -1,13 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:red_line/src/features/home/bloc/home_movie_cubit/home_movie_cubit.dart';
 
-class SearchFieldWidget extends StatelessWidget {
+class SearchFieldWidget extends StatefulWidget {
   const SearchFieldWidget({super.key});
+
+  @override
+  State<SearchFieldWidget> createState() => _SearchFieldWidgetState();
+}
+
+class _SearchFieldWidgetState extends State<SearchFieldWidget> {
+  late TextEditingController _searchController;
+
+  @override
+  void initState() {
+    _searchController = TextEditingController();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _searchController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 18, vertical: 10),
       child: TextField(
+        controller: _searchController,
         decoration: InputDecoration(
           hintText: 'Search...',
           border: OutlineInputBorder(
@@ -17,7 +39,13 @@ class SearchFieldWidget extends StatelessWidget {
           filled: true,
           fillColor: Colors.white,
           contentPadding: EdgeInsets.symmetric(horizontal: 20),
-          prefixIcon: Icon(Icons.search),
+          prefixIcon: IconButton(
+              icon: Icon(Icons.search),
+              onPressed: () {
+                context
+                    .read<HomeMovieCubit>()
+                    .loadMoviesBySearch(_searchController.text, 1);
+              }),
           suffixIcon: Row(
             mainAxisSize: MainAxisSize.min,
             children: [
