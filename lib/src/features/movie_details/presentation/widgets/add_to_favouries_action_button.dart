29aabfcfebb97f1d9e9cfdd8555915e0f5f99e9c
L4XB <lukas.buck@e-mail.de>
2025-02-16 20:07:40 +0000

    core: added some more stuff to the details page

---
+++ lib/src/features/movie_details/presentation/widgets/add_to_favouries_action_button.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:red_line/src/features/movie_details/bloc/favourites_cubit/favourites_cubit.dart';
+import 'package:red_line/src/features/movie_details/bloc/movie_details_cubit/movie_details_cubit.dart';
+
+class AddToFavouriesActionButton extends StatelessWidget {
+  const AddToFavouriesActionButton({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<MovieDetailsCubit, MovieDetailsState>(
+      builder: (context, movieDetailsState) {
+        if (movieDetailsState is MovieDetailsLoaded) {
+          final movie = movieDetailsState.movie;
+          return BlocBuilder<FavouritesCubit, List<int>>(
+            builder: (context, favouritesState) {
+              final isFavourite = favouritesState.contains(movie.id);
+              return FloatingActionButton(
+                onPressed: () {
+                  if (isFavourite) {
+                    context.read<FavouritesCubit>().removeFavourite(movie.id);
+                  } else {
+                    context.read<FavouritesCubit>().addFavourite(movie.id);
+                  }
+                },
+                backgroundColor: CupertinoColors.systemGrey5,
+                child: AnimatedSwitcher(
+                  duration: Duration(milliseconds: 300),
+                  transitionBuilder:
+                      (Widget child, Animation<double> animation) {
+                    return ScaleTransition(scale: animation, child: child);
+                  },
+                  child: Icon(
+                    isFavourite ? Icons.bookmark : Icons.bookmark_outline,
+                    color: CupertinoColors.systemYellow,
+                    size: 35,
+                    key: ValueKey<bool>(isFavourite),
+                  ),
+                ),
+              );
+            },
+          );
+        }
+        return FloatingActionButton(
+          onPressed: () {},
+          backgroundColor: CupertinoColors.systemGrey5,
+          child: Icon(
+            Icons.bookmark_outline,
+            color: CupertinoColors.systemYellow,
+            size: 35,
+          ),
+        );
+      },
+    );
+  }
+}
--- lib/src/features/movie_details/presentation/widgets/movie_details_content.dart
+++ lib/src/features/movie_details/presentation/widgets/movie_details_content.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:red_line/src/common/widgets/back_button.dart';
+import 'package:red_line/src/extensions/sized_box_extension.dart';
 import 'package:red_line/src/features/movie_details/presentation/widgets/general_informations/general_informations_and_poster.dart';
 import 'package:red_line/src/features/movie_details/presentation/widgets/general_informations/overlay_image.dart';
 
@@ -23,11 +24,27 @@ class MovieDetailsContent extends StatelessWidget {
             ),
           ],
         ),
-        Container(
-          color: Colors.red,
-          height: 100,
-          width: double.infinity,
-        )
+        SizedBoxExtension.height(40),
+        Expanded(
+          child: Container(
+            width: double.infinity,
+            decoration: BoxDecoration(
+              color: const Color.fromARGB(255, 239, 235, 235),
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(30),
+                topRight: Radius.circular(30),
+              ),
+              boxShadow: [
+                BoxShadow(
+                  color: const Color.fromARGB(255, 8, 64, 88).withAlpha(51),
+                  spreadRadius: 5,
+                  blurRadius: 7,
+                  offset: Offset(0, -3), // changes position of shadow
+                ),
+              ],
+            ),
+          ),
+        ),
       ],
     );
   }
