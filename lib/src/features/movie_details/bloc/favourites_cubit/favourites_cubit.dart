bcc6062213b5042bfb41e969391ac5926eae6ae1
L4XB <lukas.buck@e-mail.de>
2025-02-16 21:21:02 +0000

    core: added todo

--- lib/src/features/movie_details/bloc/favourites_cubit/favourites_cubit.dart
+++ lib/src/features/movie_details/bloc/favourites_cubit/favourites_cubit.dart
@@ -3,6 +3,8 @@ import 'package:bloc/bloc.dart';
 class FavouritesCubit extends Cubit<List<int>> {
   FavouritesCubit() : super([]);
 
+  /// TODO: Shared Pref und Repo einbauen
+
   void addFavourite(int movieId) {
     if (!state.contains(movieId)) {
       emit(List.from(state)..add(movieId));
