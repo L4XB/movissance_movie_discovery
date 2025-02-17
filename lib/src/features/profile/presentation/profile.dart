af45a13fdca4144fcbe21a53b8bc8f86ec230e76
L4XB <lukas.buck@e-mail.de>
2025-02-17 19:46:19 +0000

    core: finished settings section ui

--- lib/src/features/profile/presentation/profile.dart
+++ lib/src/features/profile/presentation/profile.dart
@@ -1,7 +1,7 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:red_line/src/common/extensions/sized_box_extension.dart';
 import 'package:red_line/src/features/profile/presentation/widgtes/account_bar.dart';
+import 'package:red_line/src/features/profile/presentation/widgtes/settings_section.dart';
 
 class ProfileScreen extends StatelessWidget {
   const ProfileScreen({super.key});
@@ -33,127 +33,6 @@ class ProfileScreen extends StatelessWidget {
           SizedBoxExtension.height(20),
           SettingsSection(),
         ],
-      ),
-    );
-  }
-}
-
-class SettingsSection extends StatelessWidget {
-  const SettingsSection({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    final size = MediaQuery.of(context).size;
-    return Center(
-      child: Material(
-        elevation: 3,
-        borderRadius: BorderRadius.circular(10),
-        child: Container(
-          decoration: BoxDecoration(
-            color: const Color.fromARGB(255, 230, 226, 226),
-            borderRadius: BorderRadius.circular(10),
-          ),
-          width: size.width * 0.9,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Padding(
-                padding: const EdgeInsets.symmetric(vertical: 5),
-                child: ListTile(
-                  title: Text("Dark Mode",
-                      style:
-                          TextStyle(fontSize: 15, fontWeight: FontWeight.bold)),
-                  trailing: Switch(
-                    value: true,
-                    activeColor: CupertinoColors.activeBlue,
-                    onChanged: (value) {},
-                  ),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 10),
-                child: Divider(
-                  height: 1,
-                  thickness: 1.5,
-                  color: CupertinoColors.systemGrey,
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(vertical: 5),
-                child: ListTile(
-                  title: Text("Notifications",
-                      style:
-                          TextStyle(fontSize: 15, fontWeight: FontWeight.bold)),
-                  trailing: Switch(
-                    activeColor: CupertinoColors.activeBlue,
-                    value: true,
-                    onChanged: (value) {},
-                  ),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 10),
-                child: Divider(
-                  height: 1,
-                  thickness: 1.5,
-                  color: CupertinoColors.systemGrey,
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(vertical: 5),
-                child: ListTile(
-                  title: Text("Change Profile Picture",
-                      style:
-                          TextStyle(fontSize: 15, fontWeight: FontWeight.bold)),
-                  trailing: Icon(
-                    Icons.arrow_forward_ios,
-                    size: 18,
-                  ),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 10),
-                child: Divider(
-                  height: 1,
-                  thickness: 1.5,
-                  color: CupertinoColors.systemGrey,
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(vertical: 5),
-                child: ListTile(
-                  title: Text("Reset Password",
-                      style:
-                          TextStyle(fontSize: 15, fontWeight: FontWeight.bold)),
-                  trailing: Icon(
-                    Icons.arrow_forward_ios,
-                    size: 18,
-                  ),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 10),
-                child: Divider(
-                  height: 1,
-                  thickness: 1.5,
-                  color: CupertinoColors.systemGrey,
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(vertical: 5),
-                child: ListTile(
-                  title: Text("Support",
-                      style:
-                          TextStyle(fontSize: 15, fontWeight: FontWeight.bold)),
-                  trailing: Icon(
-                    Icons.arrow_forward_ios,
-                    size: 18,
-                  ),
-                ),
-              ),
-            ],
-          ),
-        ),
       ),
     );
   }
---
+++ lib/src/features/profile/presentation/widgtes/settings_section.dart
@@ -0,0 +1,132 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class SettingsSection extends StatelessWidget {
+  const SettingsSection({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    return Center(
+      child: Material(
+        elevation: 3,
+        borderRadius: BorderRadius.circular(10),
+        child: Container(
+          decoration: BoxDecoration(
+            color: const Color.fromARGB(255, 230, 226, 226),
+            borderRadius: BorderRadius.circular(10),
+          ),
+          width: size.width * 0.9,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 5),
+                child: ListTile(
+                  title: Text("Dark Mode",
+                      style:
+                          TextStyle(fontSize: 15, fontWeight: FontWeight.bold)),
+                  trailing: Switch(
+                    value: true,
+                    activeColor: CupertinoColors.activeBlue,
+                    onChanged: (value) {},
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 10),
+                child: Divider(
+                  height: 1,
+                  thickness: 1.5,
+                  color: CupertinoColors.systemGrey,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 5),
+                child: ListTile(
+                  title: Text("Notifications",
+                      style:
+                          TextStyle(fontSize: 15, fontWeight: FontWeight.bold)),
+                  trailing: Switch(
+                    activeColor: CupertinoColors.activeBlue,
+                    value: true,
+                    onChanged: (value) {},
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 10),
+                child: Divider(
+                  height: 1,
+                  thickness: 1.5,
+                  color: CupertinoColors.systemGrey,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 5),
+                child: ListTile(
+                  title: Text("Change Profile Picture",
+                      style:
+                          TextStyle(fontSize: 15, fontWeight: FontWeight.bold)),
+                  trailing: Padding(
+                    padding: const EdgeInsets.only(right: 10),
+                    child: Icon(
+                      Icons.arrow_forward_ios,
+                      size: 18,
+                    ),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 10),
+                child: Divider(
+                  height: 1,
+                  thickness: 1.5,
+                  color: CupertinoColors.systemGrey,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 5),
+                child: ListTile(
+                  title: Text("Reset Password",
+                      style:
+                          TextStyle(fontSize: 15, fontWeight: FontWeight.bold)),
+                  trailing: Padding(
+                    padding: const EdgeInsets.only(right: 10),
+                    child: Icon(
+                      Icons.arrow_forward_ios,
+                      size: 18,
+                    ),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 10),
+                child: Divider(
+                  height: 1,
+                  thickness: 1.5,
+                  color: CupertinoColors.systemGrey,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 5),
+                child: ListTile(
+                  title: Text("Support",
+                      style:
+                          TextStyle(fontSize: 15, fontWeight: FontWeight.bold)),
+                  trailing: Padding(
+                    padding: const EdgeInsets.only(right: 10),
+                    child: Icon(
+                      Icons.arrow_forward_ios,
+                      size: 18,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
