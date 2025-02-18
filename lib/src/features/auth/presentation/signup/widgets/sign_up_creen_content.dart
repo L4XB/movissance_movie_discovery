1cbd0f173547a850cadc366ba1032968725898b6
L4XB <lukas.buck@e-mail.de>
2025-02-18 19:05:21 +0000

    core: smal refactoring

---
+++ lib/src/features/auth/presentation/signup/widgets/sign_up_creen_content.dart
@@ -0,0 +1,164 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:red_line/src/common/utils/textfield_validators.dart';
+import 'package:red_line/src/features/auth/data/auth_repository.dart';
+import 'package:red_line/src/features/auth/presentation/login/widgets/email_text_field.dart';
+import 'package:red_line/src/features/auth/presentation/login/widgets/password_text_field.dart';
+import 'package:red_line/src/features/auth/presentation/signup/widgets/image_picker_field.dart';
+import 'package:red_line/src/features/auth/presentation/signup/widgets/name_text_field.dart';
+
+class SignupScreen extends StatelessWidget {
+  final AuthRepository authRepository;
+  const SignupScreen({super.key, required this.authRepository});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Colors.blueAccent,
+        body: SignUpCreenContent(
+          authRepository: authRepository,
+        ));
+  }
+}
+
+class SignUpCreenContent extends StatefulWidget {
+  final AuthRepository authRepository;
+  const SignUpCreenContent({super.key, required this.authRepository});
+
+  @override
+  State<SignUpCreenContent> createState() => _SignUpCreenContentState();
+}
+
+class _SignUpCreenContentState extends State<SignUpCreenContent> {
+  late TextEditingController _emailController;
+  late TextEditingController _passwordController;
+  late TextEditingController _confirmPasswordController;
+  late TextEditingController _nameController;
+  final GlobalKey<FormState> formKey = GlobalKey<FormState>();
+  XFile? _selectedImage;
+
+  @override
+  void initState() {
+    _emailController = TextEditingController();
+    _passwordController = TextEditingController();
+    _confirmPasswordController = TextEditingController();
+    _nameController = TextEditingController();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _emailController.dispose();
+    _passwordController.dispose();
+    _confirmPasswordController.dispose();
+    _nameController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    return Form(
+      key: formKey,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          const SizedBox(height: 70),
+          Padding(
+            padding: const EdgeInsets.only(left: 20),
+            child: Align(
+              alignment: Alignment.centerLeft,
+              child: const Text(
+                'Create your\nAccount',
+                textAlign: TextAlign.left,
+                style: TextStyle(
+                    fontSize: 27,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white),
+              ),
+            ),
+          ),
+          const SizedBox(height: 20),
+          EmailTextField(
+            controller: _emailController,
+            validator: TextfieldValidators.emailValidator,
+          ),
+          NameTextField(
+            controller: _nameController,
+          ),
+          PasswordTextField(
+            controller: _passwordController,
+            validator: TextfieldValidators.passwordValidator,
+            label: 'Password',
+          ),
+          PasswordTextField(
+            controller: _confirmPasswordController,
+            label: 'Confirm Password',
+            validator: (value) {
+              if (value != _passwordController.text) {
+                return 'Passwords do not match';
+              }
+              return TextfieldValidators.passwordValidator(value);
+            },
+          ),
+          const SizedBox(height: 30),
+          _selectedImage != null
+              ? ClipRRect(
+                  borderRadius: BorderRadius.circular(70),
+                  child: Image.file(
+                    File(_selectedImage!.path),
+                    height: 110,
+                    width: 110,
+                    fit: BoxFit.cover,
+                  ),
+                )
+              : ClipRRect(
+                  borderRadius: BorderRadius.circular(70),
+                  child: Image.asset(
+                    'assets/images/placeholder.png',
+                    height: 110,
+                    width: 110,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+          const SizedBox(height: 10),
+          ImagePickerField(
+            onImageSelected: (image) {
+              setState(() {
+                _selectedImage = image;
+              });
+            },
+          ),
+          const SizedBox(height: 30),
+          SizedBox(
+            width: size.width * 0.9,
+            child: ElevatedButton(
+              onPressed: () {
+                final mail = _emailController.text;
+                final password = _passwordController.text;
+                final name = _nameController.text;
+                final imagePath = _selectedImage?.path;
+                if (formKey.currentState!.validate()) {
+                  widget.authRepository.signUp(mail, password, name, imagePath);
+                }
+              },
+              style: ElevatedButton.styleFrom(
+                foregroundColor: Colors.blueAccent,
+                backgroundColor: Colors.white,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(15),
+                ),
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 50, vertical: 15),
+              ),
+              child: const Text('Sign Up',
+                  style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold)),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
