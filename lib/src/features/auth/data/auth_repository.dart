4ffa21b4c23d397726ce7e407a83b57eb02a931f
L4XB <lukas.buck@e-mail.de>
2025-02-18 14:26:54 +0000

    core: added firease auth repository and implementation

---
+++ lib/src/features/auth/data/auth_repository.dart
@@ -0,0 +1,9 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
+abstract class AuthRepository {
+  Stream<User?> get onAuthStateChanged;
+  Future<void> loginUser(String email, String password);
+  Future<void> logoutUser();
+  Future<void> signUp(String email, String password);
+  Future<void> resetPassword(String email);
+}
---
+++ lib/src/features/auth/data/firebase_auth_repository.dart
@@ -0,0 +1,46 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:red_line/src/features/auth/data/auth_repository.dart';
+
+class FirebaseAuthRepository implements AuthRepository {
+  final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
+  @override
+  Future<void> loginUser(String email, String password) {
+    try {
+      return _firebaseAuth.signInWithEmailAndPassword(
+          email: email, password: password);
+    } catch (e) {
+      throw Exception(e);
+    }
+  }
+
+  @override
+  Future<void> logoutUser() {
+    try {
+      return _firebaseAuth.signOut();
+    } catch (e) {
+      throw Exception(e);
+    }
+  }
+
+  @override
+  Stream<User?> get onAuthStateChanged => _firebaseAuth.authStateChanges();
+
+  @override
+  Future<void> resetPassword(String email) {
+    try {
+      return _firebaseAuth.sendPasswordResetEmail(email: email);
+    } catch (e) {
+      throw Exception(e);
+    }
+  }
+
+  @override
+  Future<void> signUp(String email, String password) {
+    try {
+      return _firebaseAuth.createUserWithEmailAndPassword(
+          email: email, password: password);
+    } catch (e) {
+      throw Exception(e);
+    }
+  }
+}
