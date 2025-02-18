be6f702de908690cf304a27f284e730c11c5f163
L4XB <lukas.buck@e-mail.de>
2025-02-18 14:19:30 +0000

    core: some little updates for firebase ios usage

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:red_line/firebase_options.dart';
 import 'package:red_line/src/app.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(App());
 }
