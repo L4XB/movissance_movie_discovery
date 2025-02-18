8df4238385219ac8994b061f2bc9fb37b5c7a90b
L4XB <lukas.buck@e-mail.de>
2025-02-18 19:00:20 +0000

    core: updated settings screen

--- lib/src/features/profile/presentation/widgtes/account_bar.dart
+++ lib/src/features/profile/presentation/widgtes/account_bar.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -14,67 +15,75 @@ class AccountBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
-    final currentUser = context.read<UserDataCubit>().state;
-    final displayName = currentUser?.displayName ?? "";
-    final imagePath = currentUser?.photoURL;
-    return Center(
-      child: Material(
-        elevation: 3,
-        borderRadius: BorderRadius.circular(10),
-        child: Container(
-          width: size.width * 0.90,
-          decoration: BoxDecoration(
-            color: CupertinoColors.systemGrey6,
-            borderRadius: BorderRadius.circular(10),
-          ),
-          child: Padding(
-            padding: const EdgeInsets.all(12),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                CircleAvatar(
-                  radius: 30,
-                  backgroundImage: imagePath == null
-                      ? NetworkImage(
-                          "https://www.gravatar.com/avatar/2c7d99fe281ecd3bcd65ab915bac6dd5?s=250")
-                      : FileImage(File(imagePath)),
+    return BlocProvider(
+      create: (context) => UserDataCubit(),
+      child: BlocBuilder<UserDataCubit, User?>(
+        builder: (context, state) {
+          final currentUser = state;
+          final displayName = currentUser?.displayName ?? "";
+          final imagePath = currentUser?.photoURL;
+          return Center(
+            child: Material(
+              elevation: 3,
+              borderRadius: BorderRadius.circular(10),
+              child: Container(
+                width: size.width * 0.90,
+                decoration: BoxDecoration(
+                  color: CupertinoColors.systemGrey6,
+                  borderRadius: BorderRadius.circular(10),
                 ),
-                SizedBoxExtension.width(20),
-                Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.all(12),
                   child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
                     children: [
-                      Text(
-                        "Welcome, ",
-                        style: TextStyle(
-                            fontSize: 18,
-                            color: CupertinoColors.secondaryLabel),
+                      CircleAvatar(
+                        radius: 30,
+                        backgroundImage: imagePath == null
+                            ? AssetImage(
+                                "assets/images/placeholder.png",
+                              )
+                            : FileImage(File(imagePath)),
                       ),
-                      Text(
-                        displayName,
-                        style: TextStyle(
-                          fontSize: 20,
-                          fontWeight: FontWeight.bold,
-                          color: const Color.fromARGB(232, 46, 46, 48),
+                      SizedBoxExtension.width(20),
+                      Expanded(
+                        child: Row(
+                          children: [
+                            Text(
+                              "Welcome, ",
+                              style: TextStyle(
+                                  fontSize: 18,
+                                  color: CupertinoColors.secondaryLabel),
+                            ),
+                            Text(
+                              displayName,
+                              style: TextStyle(
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold,
+                                color: const Color.fromARGB(232, 46, 46, 48),
+                              ),
+                            ),
+                            Spacer(),
+                            GestureDetector(
+                              onTap: () {
+                                authRepository.logoutUser();
+                              },
+                              child: Icon(CupertinoIcons.square_arrow_right,
+                                  color: Color.fromARGB(255, 53, 65, 93),
+                                  weight: 30,
+                                  size: 25),
+                            ),
+                            SizedBoxExtension.width(10),
+                          ],
                         ),
                       ),
-                      Spacer(),
-                      GestureDetector(
-                        onTap: () {
-                          authRepository.logoutUser();
-                        },
-                        child: Icon(CupertinoIcons.square_arrow_right,
-                            color: Color.fromARGB(255, 53, 65, 93),
-                            weight: 30,
-                            size: 25),
-                      ),
-                      SizedBoxExtension.width(10),
                     ],
                   ),
                 ),
-              ],
+              ),
             ),
-          ),
-        ),
+          );
+        },
       ),
     );
   }
