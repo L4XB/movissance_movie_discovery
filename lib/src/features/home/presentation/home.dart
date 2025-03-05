4756981ff9c34c6f330550fb436a5995651c59c8
L4XB <lukas.buck@e-mail.de>
2025-03-05 16:00:53 +0000

    core: removed some uselesse code

--- lib/src/features/home/presentation/home.dart
+++ lib/src/features/home/presentation/home.dart
@@ -1,9 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:persistent_bottom_nav_bar/persistent_bottom_nav_bar.dart';
-import 'package:red_line/src/common/cubit/connectivity_cubit.dart';
 import 'package:red_line/src/common/extensions/custom_theme_colors_extension.dart';
-import 'package:red_line/src/common/widgets/connection_problems_screen.dart';
 import 'package:red_line/src/features/home/presentation/widgets/home_screen_content.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -18,24 +15,10 @@ class HomeScreen extends StatelessWidget {
     final themeExtension =
         Theme.of(context).extension<CustomThemeColorsExtension>();
 
-    return BlocListener<ConnectivityCubit, ConnectivityState>(
-      listener: (context, state) {
-        if (state is ConnectivityDisconnected) {
-          PersistentNavBarNavigator.pushNewScreen(
-            context,
-            screen: ConnectionProblemsScreen(
-              controller: controller,
-            ),
-            withNavBar: false,
-            pageTransitionAnimation: PageTransitionAnimation.cupertino,
-          );
-        }
-      },
-      child: Scaffold(
-        backgroundColor: themeExtension?.mainBackGroundColor,
-        body: HomeScreenContent(
-          controller: controller,
-        ),
+    return Scaffold(
+      backgroundColor: themeExtension?.mainBackGroundColor,
+      body: HomeScreenContent(
+        controller: controller,
       ),
     );
   }
